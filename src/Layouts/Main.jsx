5e8f742defdfe8f95f8620000cb095bda1f05029
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-06-07 02:00:18 +0600

    Start assignment

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,14 @@
+import Footer from "../Shared/Footer/Footer";
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h1>this is main </h1>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
