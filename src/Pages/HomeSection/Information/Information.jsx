521ac843b22f27582c4ad04a07da7735bac10161
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-03 13:22:29 +0600

    make info section

--- src/Pages/HomeSection/Information/Information.jsx
+++ src/Pages/HomeSection/Information/Information.jsx
@@ -2,7 +2,18 @@
 const Information = () => {
     return (
         <div className="md:grid grid-cols-4">
-            <h1>sssss</h1>
+            <div className="h-24 bg-orange-700">
+                <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, laboriosam?</h1>
+            </div>
+            <div className="h-24 bg-blue-950">
+                <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, laboriosam?</h1>
+            </div>
+            <div className="h-24 bg-orange-700">
+                <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, laboriosam?</h1>
+            </div>
+            <div className="h-24 bg-blue-950">
+                <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, laboriosam?</h1>
+            </div>
         </div>
     );
 };
