d1d2c39ef13dc643987c59ff1b92e3adfdfc3ee7
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-04 11:49:21 +0600

    fetch bloge data

--- src/components/useBlog/useBlog.jsx
+++ src/components/useBlog/useBlog.jsx
@@ -1,11 +1,11 @@
 import { useQuery } from "react-query";
 
 const useBlog = () => {
-  const { data: classes = [], refetch } = useQuery(["Courses"], async () => {
+  const { data: blogs = [], refetch } = useQuery(["blogs"], async () => {
     const res = await fetch("https://focus-studio-server.vercel.app/blogs");
     return res.json();
   });
-  return [classes, refetch];
+  return [blogs, refetch];
 };
 
 export default useBlog;
