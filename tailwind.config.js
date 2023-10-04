9f022d6e2466e8c554e49d6791724f49fb5bfcd8
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-04 10:24:42 +0600

    change cartificate css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,39 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       extend: {
+//         theme: {
+
+//         },
+//       },
+//     },
+//   },
+//   plugins: [require("daisyui")],
+//   daisyui: {
+//     themes: [{
+//       customLightTheme: {
+//         color: '#111111',
+//         background: '#ffffff',
+//         primary: '#1A0072',
+//         secondary: '#e0d1d2',
+//         accent: '#7E89A5',
+//       },
+//       customDarkTheme: {
+//         color: '#ffffff',
+//         background: '#081221',
+//         primary: '#ACBD0E',
+//         secondary: '#e0d1d2',
+//         accent: '#7E89A5',
+//       },
+//     }]
+//   },
+// }
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,10 +42,21 @@ export default {
   ],
   theme: {
     extend: {
-      extend: {
-        theme: {
-
-        },
+      customLightTheme: {
+        color: '#111111',
+        background: '#ffffff',
+        primary: '#1A0072',
+        secondary: '#e0d1d2',
+        accent: '#7E89A5',
+        textColor: '#111111', // Define text color for light theme
+      },
+      customDarkTheme: {
+        color: '#ffffff',
+        background: '#081221',
+        primary: '#ACBD0E',
+        secondary: '#e0d1d2',
+        accent: '#7E89A5',
+        textColor: '#ffffff', // Define text color for dark theme
       },
     },
   },
@@ -17,20 +64,21 @@ export default {
   daisyui: {
     themes: [{
       customLightTheme: {
-        color: '#111111',
-        background: '#f0f0f0',
+        textColor: '#111111',
+        background: '#ffffff',
         primary: '#1A0072',
-        secondary: '#354847',
-        accent: '#676f6d',
+        secondary: '#e0d1d2',
+        accent: '#7E89A5',
       },
       customDarkTheme: {
-        color: '#ffffff',
+        textColor: '#ffffff',
         background: '#081221',
         primary: '#ACBD0E',
-        secondary: '#354847',
-        accent: '#909896',
+        secondary: '#e0d1d2',
+        accent: '#7E89A5',
       },
     }]
   },
 }
+
 
