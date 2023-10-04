510a0d7d992e763454c7e244fbfcad9310cad0b0
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-04 14:11:27 +0600

    show the blog in ui

--- src/Pages/Blogs/SingleBlog.jsx
+++ src/Pages/Blogs/SingleBlog.jsx
@@ -1,5 +1,5 @@
 const SingleBlog = ({ blog }) => {
-  const { Writer_email, title, description, image, publish_date, writer_name } =
+  const { title, description, image, publish_date, writer_name } =
     blog;
   return (
     <div className="bg-white shadow-lg rounded-lg overflow-hidden">
@@ -11,17 +11,11 @@ const SingleBlog = ({ blog }) => {
         />
       </div>
       <div className="py-4 px-6">
-        <h2 className="text-2xl font-semibold text-gray-800">{title}</h2>
-        <p className="text-sm text-gray-600 mt-2">Published on {publish_date}</p>
-        <p className="text-gray-700 text-base mt-4">{description}</p>
-        <div className="mt-4">
-          <a
-            href="#"
-            className="text-blue-600 hover:underline hover:text-blue-700 text-sm font-semibold"
-          >
-            Read More
-          </a>
-        </div>
+        <h2 className="text-2xl font-semibold">{title}</h2>
+        <p className="text-sm">Published on: {publish_date}</p>
+        <p className="text-sm">Writer: {writer_name}</p>
+        <p className="text-base mt-2">{description.slice(0, 120)}</p>
+        
       </div>
     </div>
   );
